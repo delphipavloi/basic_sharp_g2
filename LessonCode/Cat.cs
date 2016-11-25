adabdcdd61c9fe60932578edb16357d7e1f78978
Pavlo Ishchenko <pavlo.ishchenko@gmail.com>
2016-11-25 14:47:20 +0200

    Add Lesson5. Class basics

---
+++ LessonCode/Cat.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LessonCode
+{
+    public class Cat
+    {
+        public static void DoYouLikeCats()
+        {
+            Console.WriteLine("Yes");
+            
+                        
+        }
+
+        public static int Popualtion { private set; get; }
+
+
+
+        private string name;
+        private int age;
+
+        public Cat()
+        {
+            name = "Unknown";
+            age = 1;
+            Breed = "Dvoryanin";
+            Popualtion++;
+        }
+
+        public Cat(string name, int age) : this()
+        {
+            this.name = name;
+            this.age = age;           
+        }
+
+        public Cat(string name, int age, string breed) : this(name, age)
+        {
+            Breed = breed;
+        }
+
+
+        public Cat(int age, string breed) : this("Tutu", age, breed)
+        {
+
+        }
+
+        public string Breed { set; get; }
+
+        public string Name
+        {
+            set
+            {
+                name = value;
+            }
+
+            get
+            {
+                return name;
+            }
+        }
+
+        public int Age
+        {
+            set
+            {                
+                if (value > 20)
+                {
+                    age = 20;
+                    return;
+                }
+                age = value;
+                //else
+                //{
+                //    age = value;
+                //}
+                                
+            }
+            get
+            {
+                return age;
+            }
+        }
+
+        public void Meow()
+        {
+            Console.WriteLine("Meow! My name is {0}, I'm {1} years old", name, age);
+        }
+
+        public void BringFlipFlop(string flipFlopColor)
+        {
+            Console.WriteLine("I'm cat, man. I can't bring this {0} flipflop", flipFlopColor);
+                   
+        }
+
+
+    }
+}
---
+++ LessonCode/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LessonCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat barsik = new Cat();
+            Cat buzefal = new Cat("Buzi", 4);
+            Cat barmoley = new Cat("Barmoley", 2, "Sphinx");
+            Cat marusya = null;
+            marusya.BringFlipFlop("Yellow");
+            barsik.Name = "Barsik";
+            barsik.Age = 200;
+            Cat.DoYouLikeCats();
+            Console.WriteLine("There are {0} cats now", Cat.Popualtion);
+            barsik.Meow();
+            barsik.BringFlipFlop("Pink with bears");
+            barmoley.BringFlipFlop("Green");
+            Console.WriteLine(barsik.Name);
+
+            Console.ReadLine();
+        }
+    }
+}
